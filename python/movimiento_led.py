ef3bfc4c233008748633fffb80cacfdc80a6e0cc
PEOPLE\pablo.aguado <aguadot@telepolis.com>
2017-10-17 13:14:52 +0200

    cambios etiquetas codigo

--- python/movimiento_led.py
+++ python/movimiento_led.py
@@ -95,7 +95,7 @@ class Example(QMainWindow):
         try:
             if sender.text() == "Conectar":
                 ser.open()
-                ex.label_1.setText("Conectado, elige laposición")
+                ex.label_1.setText("Conectado, elige la posición")
                 ex.con_serie.setText("Desconectar")
                 ex.led1.setEnabled(True)
                 ex.led2.setEnabled(True)
